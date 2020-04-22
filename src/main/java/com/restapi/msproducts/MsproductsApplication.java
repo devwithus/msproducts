a7282ddd92004f0aac31076cbef3573618353897
azhwani <azhwani@devwithus.com>
2020-04-22 18:47:37 +0100

    initial commit

---
+++ src/main/java/com/restapi/msproducts/MsproductsApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.msproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/msproducts/MsproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.msproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
